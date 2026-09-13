ee1af0701a9bfcad7ad0522a5423d628d37e2af3
arzan2475-ux <arzan2475@gmail.com>
2026-09-13 20:24:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
-  alert("Demo login button clicked!");
+  const username = document.querySelector('input[placeholder="Username"]').value;
+  const password = document.querySelector('input[placeholder="Password"]').value;
+
+  if (username === "admin" && password === "12345") {
+    alert("Login successful!");
+  } else {
+    alert("Wrong username or password!");
+  }
 });
